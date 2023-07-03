46c41834af60676cc70849553a56ebc2472ce787
Andre Paulino <andrepaulinobtw@gmail.com>
2023-07-03 17:17:49 -0300

    ✍🏽 Modularize exibirLivros.js

---
+++ scripts/exibirLivros.js
@@ -0,0 +1,25 @@
+const livrosContainer = document.querySelector('#livros');
+
+const ExibirLivros = (livros) => {
+  livros.forEach((livro) => {
+    livrosContainer.innerHTML += `
+    <div class="livro">
+      <img
+        class="livro__imagens"
+        src="${livro.imagem}"
+        alt="${livro.alt}"
+      />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+    `;
+  });
+};
+
+export default ExibirLivros;

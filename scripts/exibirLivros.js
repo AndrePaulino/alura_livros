457c78a24a88b9fd846a861d8294a11bbd79f519
Andre Paulino <andrepaulinobtw@gmail.com>
2023-07-04 01:38:29 -0300

    🗞️ Add book thumbnail availability style

--- scripts/exibirLivros.js
+++ scripts/exibirLivros.js
@@ -4,10 +4,12 @@ const ExibirLivros = (livros) => {
   livrosContainer.innerHTML = '';
 
   livros.forEach((livro) => {
+    const disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
+
     livrosContainer.innerHTML += `
     <div class="livro">
       <img
-        class="livro__imagens"
+        class="${disponibilidade}"
         src="${livro.imagem}"
         alt="${livro.alt}"
       />
